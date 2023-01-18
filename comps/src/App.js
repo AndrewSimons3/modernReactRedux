11fc88006670867b01b705151d9877f5baedf9ae
AndrewSimons3 <simdrew07@gmail.com>
2023-01-18 16:56:38 -0700

    started comps proj

---
+++ comps/src/App.js
@@ -0,0 +1,22 @@
+import Button from './Button'
+
+function App() {
+  return (
+		<div>
+			<div>
+				<Button success rounded outline>Click me!</Button>
+			</div>
+			<div>
+				<Button danger outline>Buy Now!</Button>
+			</div>
+			<div>
+				<Button warning>See Deal!</Button>
+			</div>
+			<div>
+				<Button>Hide Ads!</Button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
