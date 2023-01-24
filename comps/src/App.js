ab69a7714ec441be52be4208ae128e81b9fd97f0
AndrewSimons3 <simdrew07@gmail.com>
2023-01-24 09:25:11 -0700

    comps

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,16 +1,17 @@
-
+import Sidebar from './components/Sidebar';
 import Route from './components/Route';
 import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
-import Sidebar from './components/Sidebar';
 import ButtonPage from './pages/ButtonPage';
-
+import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
+import CounterPage from './pages/CounterPage';
 
 function App() {
-  return (
-		<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+	return (
+		<div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
 			<Sidebar />
-			<div className="col-span-5">
+			<div className='col-span-5'>
 				<Route path='/accordion'>
 					<AccordionPage />
 				</Route>
@@ -19,6 +20,15 @@ function App() {
 				</Route>
 				<Route path='/buttons'>
 					<ButtonPage />
+				</Route>
+				<Route path='/modal'>
+					<ModalPage />
+				</Route>
+				<Route path='/table'>
+					<TablePage />
+				</Route>
+				<Route path='/counter'>
+					<CounterPage initialCount={10} />
 				</Route>
 			</div>
 		</div>
--- comps/src/components/Sidebar.js
+++ comps/src/components/Sidebar.js
@@ -5,18 +5,26 @@ function Sidebar() {
 		{ label: 'Dropdown', path: '/' },
 		{ label: 'Accordion', path: '/accordion' },
 		{ label: 'Buttons', path: '/buttons' },
+		{ label: 'Modal', path: '/modal' },
+		{ label: 'Table', path: '/table' },
+		{ label: 'Counter', path: '/counter' },
 	];
 
 	const renderedLinks = links.map((link) => {
 		return (
-			<Link to={link.path} key={link.label}>
+			<Link
+				key={link.label}
+				to={link.path}
+				className='mb-3'
+				activeClassName='font-bold border-l-4 border-blue-500 pl-2'
+			>
 				{link.label}
 			</Link>
 		);
 	});
 
 	return (
-		<div className='sticky top-0 overflow-y-scroll flex flex-col'>
+		<div className='sticky top-0 overflow-y-scroll flex flex-col items-start'>
 			{renderedLinks}
 		</div>
 	);
